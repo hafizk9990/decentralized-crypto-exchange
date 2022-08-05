bc510189902442f6c431292692ce0832a2d294f2
uzair9 <studies9990@gmail.com>
2022-08-05 16:28:35 +0500

    1. Created project with all dependencies.

---
+++ hardhat.config.js
@@ -0,0 +1,7 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config(); // allows us to read our environment variables inside our HardHat project
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+};
