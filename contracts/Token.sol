a45684f681242c3a248e3b801ebb824d3d20387d
uzair9 <studies9990@gmail.com>
2022-08-09 22:14:00 +0500

    4. Completed testing the contract.

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,6 +5,20 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Token {
-    string public name = "ZarCoin";
-    string public symbol = "ZRC";
+    string public name;
+    string public symbol;
+    uint256 public decimals; // 18, because we are representing everything here in Wei (1 Ether = 10^9 GWei = 10^18 Wei)
+    uint256 public totalSupply; // A million Ethers. We will represented them in Wei.
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _decimals,
+        uint256 _totalSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+    }
 }
--- test/Token.js
+++ test/Token.js
@@ -1,20 +1,42 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Token", () => {
+describe("TOKEN", () => {
     let fetchedToken;
     let deployedToken;
-    
-    beforeEach(async () => { // this function runs before each "it( ... )"
-        fetchedToken = await ethers.getContractFactory("Token");
-        deployedToken = await fetchedToken.deploy();
-    });
-    
-    it(`has correct name`, async () => {
-        expect(await deployedToken.name()).to.equal("ZarCoin");
-    });
 
-    it(`has correct symbol`, async () => {
-        expect(await deployedToken.symbol()).to.equal("ZRC");
+    function ethersToWei(n) {
+        // Returns equivalent Wei for inputted Ethers.
+
+        return(ethers.utils.parseUnits(n.toString(), "ether")); 
+    }
+    
+    beforeEach(async () => { // This hook runs before each "it( ... )"
+        fetchedToken = await ethers.getContractFactory("Token");
+        deployedToken = await fetchedToken.deploy("Uzair", "UZR", 18, ethersToWei(1000000));
+        // ^ Supplying params to the constructor of the contract during its deployment.
+    });
+    
+    describe("1) Deployment:", () => {
+        const name = "Uzair";
+        const symbol = "UZR";
+        const decimals = "18";
+        const totalSupply = ethersToWei(1000000);
+        
+        it(`has correct name`, async () => {
+            expect(await deployedToken.name()).to.equal(name);
+        });
+    
+        it(`has correct symbol`, async () => {
+            expect(await deployedToken.symbol()).to.equal(symbol);
+        });
+    
+        it('has correct decimals', async () => {
+            expect(await deployedToken.decimals()).to.equal(decimals);
+        });
+    
+        it('has correct total supply', async () => {
+            expect(await deployedToken.totalSupply()).to.equal(totalSupply); // Equal to this many Wei: "1000000000000000000000000"
+        });
     });
 });
