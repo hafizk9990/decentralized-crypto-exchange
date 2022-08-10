c5e08031861ce926e7fe6b3f7263e572bc992ee9
uzair9 <studies9990@gmail.com>
2022-08-10 18:19:06 +0500

    5. balanceOf( ... ) implemented and tested.

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,6 +9,7 @@ contract Token {
     string public symbol;
     uint256 public decimals; // 18, because we are representing everything here in Wei (1 Ether = 10^9 GWei = 10^18 Wei)
     uint256 public totalSupply; // A million Ethers. We will represented them in Wei.
+    mapping(address => uint256) public balanceOf;
 
     constructor(
         string memory _name,
@@ -20,5 +21,13 @@ contract Token {
         symbol = _symbol;
         decimals = _decimals;
         totalSupply = _totalSupply;
+
+        /*
+            Give all the tokens to the deployer of the smart contract.
+            Here, msg is a global variable.
+            msg.sender refers to the deployer's wallet address
+        */
+
+        balanceOf[msg.sender] = totalSupply;
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -2,8 +2,7 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("TOKEN", () => {
-    let fetchedToken;
-    let deployedToken;
+    let fetchedToken, deployedToken, allAccounts, deployersAddress;
 
     function ethersToWei(n) {
         // Returns equivalent Wei for inputted Ethers.
@@ -15,9 +14,11 @@ describe("TOKEN", () => {
         fetchedToken = await ethers.getContractFactory("Token");
         deployedToken = await fetchedToken.deploy("Uzair", "UZR", 18, ethersToWei(1000000));
         // ^ Supplying params to the constructor of the contract during its deployment.
+        allAccounts = await ethers.getSigners();
+        deployersAddress = allAccounts[0].address; // The first (out of the 20 given) accounts is the deployer.
     });
     
-    describe("1) Deployment:", () => {
+    describe("01. Deployment:", () => {
         const name = "Uzair";
         const symbol = "UZR";
         const decimals = "18";
@@ -37,6 +38,10 @@ describe("TOKEN", () => {
     
         it('has correct total supply', async () => {
             expect(await deployedToken.totalSupply()).to.equal(totalSupply); // Equal to this many Wei: "1000000000000000000000000"
+        });
+
+        it('has assigned total supply to the deployer', async() => {
+            expect(await deployedToken.balanceOf(deployersAddress)).to.equal(totalSupply);
         });
     });
 });
