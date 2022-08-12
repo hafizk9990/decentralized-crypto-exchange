25abe7675a63ff66560e117634fb4afc85bb2478
uzair9 <studies9990@gmail.com>
2022-08-12 18:31:46 +0500

    7. Created allowance mapping and approve( ... ) with tests.

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,8 +10,15 @@ contract Token {
     uint256 public decimals; // 18, because we are representing everything here in Wei (1 Ether = 10^9 GWei = 10^18 Wei)
     uint256 public totalSupply; // A million Ethers. We will represented them in Wei.
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 
     constructor(
         string memory _name,
@@ -37,11 +44,25 @@ contract Token {
         public
         returns (bool success)
     {
-        require(balanceOf[msg.sender] >= _value);
+        require(balanceOf[msg.sender] >= _value && _to != address(0));
+        require(_to != address(0));
+
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(_spender != address(0));
+
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
 
         return true;
     }
--- test/Token.js
+++ test/Token.js
@@ -2,12 +2,11 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("TOKEN", () => {
-    let fetchedToken, deployedToken, allAccounts, deployer, receiver;
+    let fetchedToken, deployedToken, transaction, result;
+    let allAccounts, deployer, receiver, exchange;
 
     function ethersToWei(n) {
-        // Returns equivalent Wei for inputted Ethers.
-
-        return(ethers.utils.parseUnits(n.toString(), "ether")); 
+        return(ethers.utils.parseUnits(n.toString(), "ether")); // Return equivalent Wei for inputted Ethers.
     }
     
     beforeEach(async () => { // This hook runs before each "it( ... )"
@@ -15,10 +14,11 @@ describe("TOKEN", () => {
         deployedToken = await fetchedToken.deploy("Uzair", "UZR", 18, ethersToWei(1000000)); // ^ Supplying params to the constructor of the contract during its deployment.
         allAccounts = await ethers.getSigners(); // Get all 20 accounts.
         deployer = allAccounts[0]; // The first (out of the 20 given) account is that of the deployer's.
-        receiver = allAccounts[1];
+        receiver = allAccounts[1]; // Let us pretend the second address to be our receiver.
+        exchange = allAccounts[2]; // Predending the third address to be that of the exchange's.
     });
     
-    describe("DEPLOYMENT:", () => {
+    describe("Deployment", () => {
         const name = "Uzair";
         const symbol = "UZR";
         const decimals = "18";
@@ -45,10 +45,8 @@ describe("TOKEN", () => {
         });
     });
 
-    describe("SENDING TOKENS:", () => {
-        let transaction, result;
-        
-        describe("SUCCESS", () => {
+    describe("Token Transfer", () => {
+        describe("Success", () => {
             beforeEach(async() => {
                 transaction = await deployedToken.transfer(receiver.address, ethersToWei(100));
                 result = await transaction.wait();
@@ -69,9 +67,41 @@ describe("TOKEN", () => {
             });
         });
 
-        describe("FAILURE", () => {
-            it('rejects insufficient funds', async() => {
+        describe("Failure", () => {
+            it('rejects transfer of insufficient funds', async() => {
                 await expect(deployedToken.transfer(receiver.address, ethersToWei(1000000000))).to.be.reverted;
+            });
+            
+            it("rejects invalid recepient", async() => {
+                await expect(deployedToken.approve("0x0000000000000000000000000000000000000000", ethersToWei(1000000000))).to.be.reverted;
+            });
+        });
+    });
+
+    describe("Token Approvals", () => {
+        beforeEach(async() => {
+            transaction = await deployedToken.approve(exchange.address, ethersToWei(100));
+            result = await transaction.wait();
+        });
+        
+        describe("Success", () => {
+            it("allocates an allowance to the spender", async() => {
+                expect(await deployedToken.allowance(deployer.address, exchange.address)).to.equal(ethersToWei(100));
+            });
+            
+            it(`emits the "Approval" event with correct details`, async() => {
+                const emittedEvent = result.events[0];
+    
+                expect(emittedEvent.event).to.equal("Approval");
+                expect(emittedEvent.args.owner).to.equal(deployer.address);
+                expect(emittedEvent.args.spender).to.equal(exchange.address);
+                expect(emittedEvent.args.value).to.equal(ethersToWei(100));
+            });
+        });
+
+        describe("Failure", () => {
+            it("rejects invalid spender", async() => {
+                await expect(deployedToken.approve("0x0000000000000000000000000000000000000000", ethersToWei(1000000000))).to.be.reverted;
             });
         });
     });
