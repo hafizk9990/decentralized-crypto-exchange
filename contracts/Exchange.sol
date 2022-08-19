db1332e16a678f6fa61b8c255806c029f2353568
uzair9 <studies9990@gmail.com>
2022-08-19 23:17:52 +0500

    13. makeOrder( ... ) implemented and tested.

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -6,6 +6,7 @@ import "./Token.sol";
 contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
+    uint256 public orderNumber;
     
     /*
         The mapping down below keeps track of which smart contract's
@@ -14,7 +15,18 @@ contract Exchange {
     */
     
     mapping(address => mapping(address => uint256)) public balanceOf;
+    mapping(uint256 => _Order) public orders;
 
+    struct _Order {
+        uint256 id;
+        address user;
+        address tokenGive;
+        uint256 amountGive;
+        address tokenGet;
+        uint256 amountGet;
+        uint256 timestamp;
+    }
+    
     event Deposit(
         address smartContract,
         address user,
@@ -29,11 +41,27 @@ contract Exchange {
         uint256 balance 
     );
     
+    event Order(
+        uint256 id,
+        address user,
+        address tokenGive,
+        uint256 amountGive,
+        address tokenGet,
+        uint256 amountGet,
+        uint256 timestamp
+    );
+    
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
+        orderNumber = 0;
     }
 
+    /*
+        The function deposit( ... ) allows a user to list
+        their crypto on the exchange.
+    */
+    
     function deposit(address _externalSmartContract, uint256 _deposittedAmount)
         public
     {
@@ -62,16 +90,64 @@ contract Exchange {
         emit Deposit(_externalSmartContract, msg.sender,_deposittedAmount, balanceOf[_externalSmartContract][msg.sender]);
     }
 
+    /*
+        The function withdraw( ... ) allows a user to take their 
+        listed crypto out of the exchange, as they no longer want
+        to trade on this exchange.
+
+        Why?
+
+        Well, maybe they found another exchange with better rates
+        for trading.
+    */
+
     function withdraw(address _externalSmartContract, uint256 _withdrawalAmount)
         public
     {
         require(
             balanceOf[_externalSmartContract][msg.sender] >= _withdrawalAmount, 
-            "You have depositted NO crypto funds to the exchange yet. Therefore, your request for a withdrawal has been rejected."
+            "ERROR: You have depositted NO crypto funds to the exchange yet. Therefore, your request for a withdrawal has been rejected."
         );
         
         Token(_externalSmartContract).transfer(msg.sender, _withdrawalAmount);
         balanceOf[_externalSmartContract][msg.sender] -= _withdrawalAmount;
         emit Withdraw(_externalSmartContract, msg.sender, _withdrawalAmount, balanceOf[_externalSmartContract][msg.sender]);
+    }
+    
+    /*
+        Making order means asking one of the users to put their
+        crypto up for trade at a specific rate.
+
+        _amountGive and _amoungGet give us the rate at which the 
+        maker of the trade is trying to cut a deal.
+    */
+    
+    function makeOrder(address _tokenGive, uint256 _amountGive, address _tokenGet, uint256 _amountGet)
+        public
+    {
+        require(
+            balanceOf[_tokenGive][msg.sender] >= _amountGive,
+            "ERROR: Depositted funds are insufficient for this trade to take place."
+        );
+        
+        orders[orderNumber] = _Order(
+            orderNumber,
+            msg.sender, 
+            _tokenGive,
+            _amountGive,
+            _tokenGet,
+            _amountGet,
+            block.timestamp
+        );
+
+        emit Order(
+            orderNumber++,
+            msg.sender, 
+            _tokenGive,
+            _amountGive,
+            _tokenGet,
+            _amountGet,
+            block.timestamp
+        );
     }
 }
--- test/Exchange.js
+++ test/Exchange.js
@@ -2,7 +2,7 @@ const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
 describe("Exchange", () => {
-    let fetchedExchange, deployedExchange, fetchedToken, deployedToken;
+    let fetchedExchange, deployedExchange, fetchedToken, deployedToken1, deployedToken2;
     let allAccounts, deployer, feeAccount, user1;
     let transaction, result;
 
@@ -20,7 +20,8 @@ describe("Exchange", () => {
         deployedExchange = await fetchedExchange.deploy(feeAccount.address, 1);
 
         fetchedToken = await ethers.getContractFactory("Token");
-        deployedToken = await fetchedToken.deploy("Uzair", "UZR", 18, ethersToWei(1000000));
+        deployedToken1 = await fetchedToken.deploy("Uzair", "UZR", 18, ethersToWei(1000000));
+        deployedToken2 = await fetchedToken.deploy("Mock DAI", "mDAI", 18, ethersToWei(1000000));
 
         /*
             Below, we are allowing the deployer of Token to create
@@ -32,7 +33,7 @@ describe("Exchange", () => {
             carried out by user1, not the deployer.
         */
         
-        await deployedToken.connect(deployer).transfer(user1.address, ethersToWei(100));
+        await deployedToken1.connect(deployer).transfer(user1.address, ethersToWei(100));
     });
     
     it("adds correct account as the feeAccount", async() => {
@@ -46,19 +47,19 @@ describe("Exchange", () => {
     describe("Token Deposits", () => {
         describe("Success", () => {
             beforeEach(async() => {
-                await deployedToken.connect(user1).approve(deployedExchange.address, ethersToWei(100));
-                transaction = await deployedExchange.connect(user1).deposit(deployedToken.address, ethersToWei(100));
+                await deployedToken1.connect(user1).approve(deployedExchange.address, ethersToWei(100));
+                transaction = await deployedExchange.connect(user1).deposit(deployedToken1.address, ethersToWei(100));
                 result = await transaction.wait();
             });
 
             it("tracks token deposits correctly", async() => {
-                expect(await deployedToken.balanceOf(user1.address)).to.equal(ethersToWei(0));
-                expect(await deployedToken.balanceOf(deployedExchange.address)).to.equal(ethersToWei(100));
-                expect(await deployedExchange.balanceOf(deployedToken.address, user1.address)).to.equal(ethersToWei(100));
+                expect(await deployedToken1.balanceOf(user1.address)).to.equal(ethersToWei(0));
+                expect(await deployedToken1.balanceOf(deployedExchange.address)).to.equal(ethersToWei(100));
+                expect(await deployedExchange.balanceOf(deployedToken1.address, user1.address)).to.equal(ethersToWei(100));
             });
             
             it(`emits the "Deposit" event correctly`, async() => {
-                expect(result.events[1].args.smartContract).to.equal(deployedToken.address);
+                expect(result.events[1].args.smartContract).to.equal(deployedToken1.address);
                 expect(result.events[1].args.user).to.equal(user1.address);
                 expect(result.events[1].args.amount).to.equal(ethersToWei(100));
                 expect(result.events[1].args.balance).to.equal(ethersToWei(100));
@@ -68,7 +69,7 @@ describe("Exchange", () => {
 
         describe("Failure", () => {
             it("fails if there is no approval for token deplosit", async() => {
-                await expect(deployedExchange.connect(user1).deposit(deployedToken.address, ethersToWei(100))).to.be.reverted;
+                await expect(deployedExchange.connect(user1).deposit(deployedToken1.address, ethersToWei(100))).to.be.reverted;
             });
         });
     });
@@ -76,21 +77,21 @@ describe("Exchange", () => {
     describe("Token Withdrawal", () => {
         describe("Success", () => {
             beforeEach(async() => {
-                await deployedToken.connect(user1).approve(deployedExchange.address, ethersToWei(100));
-                await deployedExchange.connect(user1).deposit(deployedToken.address, ethersToWei(100));
+                await deployedToken1.connect(user1).approve(deployedExchange.address, ethersToWei(100));
+                await deployedExchange.connect(user1).deposit(deployedToken1.address, ethersToWei(100));
             });
 
             it("tracks token withdrawal correctly", async() => {
-                transaction = await deployedExchange.connect(user1).withdraw(deployedToken.address, ethersToWei(100));
+                transaction = await deployedExchange.connect(user1).withdraw(deployedToken1.address, ethersToWei(100));
                 result = await transaction.wait();
                 
-                expect(await deployedToken.balanceOf(user1.address)).to.equal(ethersToWei(100));
-                expect(await deployedToken.balanceOf(deployedExchange.address)).to.equal(ethersToWei(0));
-                expect(await deployedExchange.balanceOf(deployedToken.address, user1.address)).to.equal(ethersToWei(0));
+                expect(await deployedToken1.balanceOf(user1.address)).to.equal(ethersToWei(100));
+                expect(await deployedToken1.balanceOf(deployedExchange.address)).to.equal(ethersToWei(0));
+                expect(await deployedExchange.balanceOf(deployedToken1.address, user1.address)).to.equal(ethersToWei(0));
             });
             
             it(`emits the "Withdrawal" event correctly`, async() => {
-                // expect(result.events[1].args.smartContract).to.equal(deployedToken.address);
+                // expect(result.events[1].args.smartContract).to.equal(deployedToken1.address);
                 expect(result.events[1].args.user).to.equal(user1.address);
                 expect(result.events[1].args.amount).to.equal(ethersToWei(100));
                 expect(result.events[1].args.balance).to.equal(ethersToWei(0));
@@ -100,7 +101,43 @@ describe("Exchange", () => {
 
         describe("Failure", () => {
             it("fails to withdraw without a deposit first", async() => {
-                await expect(deployedExchange.connect(user1).withdraw(deployedToken.address, ethersToWei(100))).to.be.reverted;
+                await expect(deployedExchange.connect(user1).withdraw(deployedToken1.address, ethersToWei(100))).to.be.reverted;
+            });
+        });
+    });
+    
+    
+    describe("Making Orders", () => {
+        describe("Success", () => {
+            beforeEach(async() => {
+                await deployedToken1.connect(user1).approve(deployedExchange.address, ethersToWei(100));
+                await deployedExchange.connect(user1).deposit(deployedToken1.address, ethersToWei(100));
+            
+                transaction = await deployedExchange.connect(user1).makeOrder(deployedToken1.address, ethersToWei(1), deployedToken2.address, ethersToWei(1));
+                result = await transaction.wait();
+            });
+
+            it("makes orders correctly", async() => {
+                expect(await deployedExchange.orderNumber()).to.equal(1);
+            });
+            
+            it(`emits the "Make Order" event correctly`, async() => {
+                expect(result.events[0].event).to.equal("Order");
+                expect(result.events[0].args.id).to.equal(0);
+                expect(result.events[0].args.user).to.equal(user1.address);
+                expect(result.events[0].args.amountGive).to.equal(ethersToWei(1));
+                expect(result.events[0].args.amountGet).to.equal(ethersToWei(1));
+                expect(result.events[0].args.tokenGet).to.equal(deployedToken2.address);
+                expect(result.events[0].args.tokenGive).to.equal(deployedToken1.address);
+                expect(result.events[0].args.timestamp).to.at.least(1);
+            });
+        });
+
+        describe("Failure", () => {
+            it("fails to make orders with insufficient funds", async() => {
+                await deployedToken1.connect(user1).approve(deployedExchange.address, ethersToWei(10));
+                await deployedExchange.connect(user1).deposit(deployedToken1.address, ethersToWei(10));
+                await expect(deployedExchange.connect(user1).makeOrder(deployedToken1.address, ethersToWei(100), deployedToken2.address, ethersToWei(1))).to.be.reverted;
             });
         });
     });
