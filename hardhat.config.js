4c85fc15adf4b09d426b152e085965ae58a8f80e
Uzair Afzal <studies9990@gmail.com>
2022-10-13 17:09:41 +0500

    Goerli deployment - 01

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config(); // This allows us to read our environment variables inside our HardHat project
+const privateKeys = process.env.PRIVATE_KEYS || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -8,5 +9,9 @@ module.exports = {
     localhost: {
         url: "http://127.0.0.1:8545"
     },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: privateKeys.split(","),
+    }
   }
 };
